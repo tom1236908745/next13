ef93ad22651ed147f2e118d0face6ab1c0e6d9ce
tom1236908745 <tom1233908745@gmail.com>
2022-11-03 13:15:57 +0900

    add: レイアウトファイルに状態を持たせた

---
+++ app/dir1/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <>
+      <h1>Hello, dir 1</h1>
+    </>
+  );
+};
+
+export default Page;
---
+++ app/dir2/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <>
+      <h1>Hello, dir 2</h1>
+    </>
+  );
+};
+
+export default Page;
